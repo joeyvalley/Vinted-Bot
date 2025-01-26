6ecbadd994aa364d96254c2306ee54f1f0058105
malthe lund madsen <malthelm@gmail.com>
2025-01-25 17:38:57 -0800

    chore: remove old JavaScript files

--- src/config/env.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-
-export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-export const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
-export const REDIS_PORT = process.env.REDIS_PORT || 6379;
-export const PROXY_URL = process.env.PROXY_URL;
